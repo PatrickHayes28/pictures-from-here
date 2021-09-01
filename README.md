6982d918b6787e7775f8b176885a5ae74bba0bfa
PatrickHayes28 <patrickh61@yahoo.com>
2021-08-31 21:25:24 -0400

    added comment to the READme folder

--- README.md
+++ README.md
@@ -1,5 +1,51 @@
-# Kenzie Academy JavaScript Assignment
+## Photos From Here Development Plan:
 
-Complete your work inside of `code.js`
+A. Get location that we want to see photos of
 
-Follow the instructions provided on my.kenzie.academy for this assignment.
+1. use Geolocation API to get coordinates (lat and lon) or use a fallback location
+
+
+    - [ this is a link to the documentation ]
+
+B. Get photo info from Flickr
+
+1. use fetch() to send a GET request to flickr.com/services/rest
+
+
+    - Include the lat and lon
+    - Include a search term
+
+2. Process the promises to get the photo data
+
+
+    - Convert JSON to a usable object ("rehydrate")
+    - Send the photo data to a display function
+
+C. Display photos
+
+1. Create the image URLs from the photo data (function)
+
+
+    - https://www.flickr.com/services/api/misc.urls.html
+
+2. Insert an <img> tag into the page
+
+
+    - crate an img element
+    - set src attribute to image URL
+    - append the element to the right place in the document
+
+3. Display link to the image's Flickr page (function)
+
+
+    - (Same stuff as the img tag)
+
+D. Provide a way to advance through photos
+
+1. Get the geographic location from the browser
+2. Construct the query URL
+3. Use fetch to send the request to Flickr
+4. Process the response data into an object
+5. Use the values in the response object to construct an image source URL
+6. Display the first image on the page
+7. In response to some event (e.g. a button click or a setInterval), show the next image in the collection
--- code.js
+++ code.js
@@ -4,7 +4,7 @@
 //this is my key to use the flickr api
 //Key:
 //c30e406bc6b257d9450039b95efe1416
-//this secret came with the api key                 //im getting images related to my location but when I get to the end of the set clicking next photo doesn't go back to the first photo
+//this secret came with the api key
 //Secret:
 //78ab41834364bee8
 
@@ -141,35 +141,3 @@ function onError() {
 }
 
 navigator.geolocation.watchPosition(onSuccess, onError);
-
-/*
-Photos From Here Development Plan:
-----------------------------------
-A. Get location that we want to see photos of
-  1. use Geolocation API to get coordinates (lat and lon) or use a fallback location
-	- [ this is a link to the documentation ]
-B. Get photo info from Flickr
-  1. use fetch() to send a GET request to flickr.com/services/rest
-	- Include the lat and lon
-	- Include a search term
-  2. Process the promises to get the photo data
-	- Convert JSON to a usable object ("rehydrate")
-	- Send the photo data to a display function
-C. Display photos
-  1. Create the image URLs from the photo data (function)
-	- https://www.flickr.com/services/api/misc.urls.html
-  2. Insert an <img> tag into the page
-	- crate an img element
-	- set src attribute to image URL
-	- append the element to the right place in the document
-  3. Display link to the image's Flickr page (function)
-	- (Same stuff as the img tag)
-D. Provide a way to advance through photos
-1. Get the geographic location from the browser
-2. Construct the query URL
-3. Use fetch to send the request to Flickr
-4. Process the response data into an object
-5. Use the values in the response object to construct an image source URL
-6. Display the first image on the page
-7. In response to some event (e.g. a button click or a setInterval), show the next image in the collection
-*/
